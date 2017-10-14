1d62673a97f7566dad9f6ae0b3355a912e2a7b67
Jérémy Buget <jbuget@gmail.com>
2017-10-14 15:59:52 +0200

    Improve code structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const server = require('./lib/server');
+const plugins = require('./lib/plugins');
+
+server.register(plugins, (err) => {
+
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
